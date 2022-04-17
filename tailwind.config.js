dda4158ae146e5d65bd52c12d6e1721aafce88c8
Pierre-François Salmand <maabaa@gmail.com>
2022-04-18 02:04:51 +0700

    import assets

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,21 @@
 module.exports = {
   content: [
-    "./pages/**/*.{js,ts,jsx,tsx}",
-    "./components/**/*.{js,ts,jsx,tsx}",
+    "./pages/**/*.{js,jsx,ts,tsx}",
+    "./components/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        courgette: ["Courgette", "cursive"],
+        baskerville: ["LibreBaskerville", "serif"],
+      },
+      backgroundImage: {
+        "lc-main": "url('../public/img/Main.webp')",
+      },
+      colors: {
+        "lc-red": "#990022",
+      },
+    },
+    plugins: [],
   },
-  plugins: [],
 }
