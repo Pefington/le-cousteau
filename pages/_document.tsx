ead73a68fe8e48a9fffa4d17533bb2543169e62b
Pierre-François Salmand <maabaa@gmail.com>
2022-04-18 16:58:34 +0700

    Fix crossOrigin type

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -10,7 +10,11 @@ class MyDocument extends Document {
     return (
       <Html style={{ height: "-webkit-fill-available" }}>
         <Head>
-          <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin />
+          <link
+            rel="preconnect"
+            href="https://fonts.gstatic.com"
+            crossOrigin=""
+          />
           <link
             href="https://fonts.googleapis.com/css2?family=Courgette&family=Libre+Baskerville:ital,wght@0,400;0,700;1,400&display=swap"
             rel="stylesheet"
