330866bf3a94041921080e2ec63020db89f22317
Pierre-François Salmand <maabaa@gmail.com>
2022-04-26 12:21:36 +0700

    Implement multilingual template

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -8,20 +8,6 @@ const Footer: NextComponentType = () => {
   const t = useTranslation()
 
   return (
-    //   <Link
-    //     href="https://www.gites-de-france.com/fr/occitanie/gers/chambres-au-cousteau-32g100039"
-    //     passHref
-    //   >
-    //     <a target="_blank" className="h-10 w-10 opacity-60 transition-opacity">
-    //       <Image
-    //         src="/svg/Gites-de-France.svg"
-    //         alt="Logo Gîtes de France"
-    //         width="100%"
-    //         height="100%"
-    //       />
-    //     </a>
-    //   </Link>
-
     <footer className="fixed bottom-0 flex flex-col items-center p-4 text-sm text-neutral-content">
       <h4>Auberge Le Cousteau – 32360 Saint-Lary</h4>
       <div className="flex">
--- components/Header.tsx
+++ components/Header.tsx
@@ -1,10 +1,12 @@
 import { NextComponentType } from "next"
-import template from "../public/locales/fr/template.json"
-import { useTranslation } from "next-i18next"
+import template from "../template/template.json"
 import LanguageSelector from "./LanguageSelector"
+import Image from "next/image"
+import Link from "next/link"
+import { useRouter } from "next/router"
 
 const Header: NextComponentType = () => {
-  const { t } = useTranslation()
+  const { locale } = useRouter()
   return (
     <nav className="navbar fixed top-0 z-50 text-neutral-content transition-all delay-300">
       <div className="navbar-start">
@@ -29,75 +31,55 @@ const Header: NextComponentType = () => {
             tabIndex={0}
             className="dropdown-content menu rounded-box menu-compact mt-3 w-52 p-2 shadow"
           >
-            {template.sections.map((section, i) =>
-              !section.subsections ? (
-                <li key={i}>
-                  <h2>{section.name}</h2>
-                </li>
-              ) : (
-                <li key={i} tabIndex={0}>
-                  <a className="justify-between">
-                    <h2>{section.name}</h2>
-                    <svg
-                      className="fill-current"
-                      xmlns="http://www.w3.org/2000/svg"
-                      width="24"
-                      height="24"
-                      viewBox="0 0 24 24"
-                    >
-                      <path d="M8.59,16.58L13.17,12L8.59,7.41L10,6L16,12L10,18L8.59,16.58Z" />
-                    </svg>
-                  </a>
-                  <ul className="p-2">
-                    {section.subsections.map((subsection, i) => (
-                      <li key={i}>
-                        <a>
-                          <h3>{subsection.name}</h3>
-                        </a>
-                      </li>
-                    ))}
-                  </ul>
-                </li>
-              )
+            {template.sections.map(
+              (section, i) =>
+                (locale === "fr" && (
+                  <li key={i}>
+                    <h2>{section.fr}</h2>
+                  </li>
+                )) ||
+                (locale === "en" && (
+                  <li key={i}>
+                    <h2>{section.en}</h2>
+                  </li>
+                ))
             )}
           </ul>
         </div>
         <a className="btn btn-ghost text-xl normal-case">
           <h1>Le Cousteau</h1>
         </a>
+        <Link
+          href="https://www.gites-de-france.com/fr/occitanie/gers/chambres-au-cousteau-32g100039"
+          passHref
+        >
+          <a
+            target="_blank"
+            className="h-10 w-10 opacity-60 transition-opacity"
+          >
+            <Image
+              src="/svg/Gites-de-France.svg"
+              alt="Logo Gîtes de France"
+              width="100%"
+              height="100%"
+            />
+          </a>
+        </Link>
       </div>
       <div className="navbar-center hidden lg:flex">
         <ul className="menu menu-horizontal p-0">
-          {template.sections.map((section, i) =>
-            !section.subsections ? (
-              <li key={i}>
-                <h2>{section.name}</h2>
-              </li>
-            ) : (
-              <li key={i} tabIndex={0}>
-                <a>
-                  <h2>{section.name}</h2>
-                  <svg
-                    className="fill-current"
-                    xmlns="http://www.w3.org/2000/svg"
-                    width="20"
-                    height="20"
-                    viewBox="0 0 24 24"
-                  >
-                    <path d="M7.41,8.58L12,13.17L16.59,8.58L18,10L12,16L6,10L7.41,8.58Z" />
-                  </svg>
-                </a>
-                <ul className="p-2">
-                  {section.subsections.map((subsection, i) => (
-                    <li key={i}>
-                      <a>
-                        <h3>{subsection.name}</h3>
-                      </a>
-                    </li>
-                  ))}
-                </ul>
-              </li>
-            )
+          {template.sections.map(
+            (section, i) =>
+              (locale === "fr" && (
+                <li key={i}>
+                  <h2>{section.fr}</h2>
+                </li>
+              )) ||
+              (locale === "en" && (
+                <li key={i}>
+                  <h2>{section.en}</h2>
+                </li>
+              ))
           )}
         </ul>
       </div>
