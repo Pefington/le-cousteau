aba2b416c8d6d423ba4c22eb2a040f62bcd18949
Pierre-François Salmand <maabaa@gmail.com>
2022-05-19 03:51:28 +0700

    Add more pages

---
+++ pages/tarifs.tsx
@@ -0,0 +1,53 @@
+import type { NextPage } from "next"
+import Head from "next/head"
+import Image from "next/image"
+import { useTranslation } from "next-i18next"
+import { serverSideTranslations } from "next-i18next/serverSideTranslations"
+
+type staticProps = {
+  locale: string
+}
+
+export async function getStaticProps({ locale }: staticProps) {
+  return {
+    props: {
+      ...(await serverSideTranslations(locale, ["common"])),
+      // Will be passed to the page component as props
+    },
+  }
+}
+
+const Tarifs: NextPage = () => {
+  const { t } = useTranslation()
+
+  return (
+    <>
+      <Head>
+        <title>Nos Prestations</title>
+        <meta name="description" content="" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <div className="hero min-h-screen bg-lc-main">
+        <div className="hero-overlay bg-opacity-60"></div>
+        <main className="hero-content text-center text-neutral-content">
+          <div className="z-40 max-w-md">
+            <Image
+              src="https://source.unsplash.com/collection/1027750/1024x768"
+              alt=""
+              width="1024"
+              height="768"
+              // layout="responsive"
+            />
+            <p className="mb-5">
+              {t("PRESTATIONS.01-LeLieuEstÉgalementAdapté")}
+            </p>
+            <p className="mb-5">{t("PRESTATIONS.02-Jusque30Couchages")}</p>
+            <p className="mb-5">{t("PRESTATIONS.03-ComposerAvecLeChef")}</p>
+          </div>
+        </main>
+      </div>
+    </>
+  )
+}
+
+export default Tarifs
