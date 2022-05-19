188fd921ec2a6168d3d8cfd17b25cfe63a7688d2
Pierre-François Salmand <maabaa@gmail.com>
2022-05-20 01:17:35 +0700

    Fix TS linting issue

--- next.config.js
+++ next.config.js
@@ -1,9 +1,11 @@
-import { i18n } from "./next-i18next.config"
+/* eslint @typescript-eslint/no-var-requires: "off" */
 /** @type {import('next').NextConfig} */
+
+const { i18n } = require("./next-i18next.config")
 
 module.exports = {
   images: {
-    domains: ["source.unsplash.com"],
+    domains: ["source.unsplash.com", "cdnjs.cloudflare.com"],
   },
   nextConfig: {
     reactStrictMode: true,
