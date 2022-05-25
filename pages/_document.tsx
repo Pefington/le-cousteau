b6779b76fa0206c85c72abb38eec53c93ad9502a
Pierre-François Salmand <maabaa@gmail.com>
2022-05-26 03:04:49 +0700

    Remove preconnect for gfonts

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -9,11 +9,6 @@ export default function Document() {
   return (
     <Html style={{ height: "-webkit-fill-available" }}>
       <Head>
-        {/* <link
-          rel="preconnect"
-          href="https://fonts.gstatic.com"
-          crossOrigin=""
-        /> */}
         <link
           href="https://fonts.googleapis.com/css2?family=Courgette&family=Libre+Baskerville:ital,wght@0,400;0,700;1,400&display=swap"
           rel="stylesheet"
